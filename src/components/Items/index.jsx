dc21bf0dfc042addd05ac1f17e26e3291bcd5f92
zennarchi <art256100@gmail.com>
2019-10-23 19:38:50 +0300

    Fix one import

--- src/components/Items/index.jsx
+++ src/components/Items/index.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './index.css';
-import AddComment from "../AddItem";
+import AddItem from "../AddItem";
 import Item from "../Item";
 
 const Items = (props) => (
     <div className='shadow mb-5 bg-white items'>
         <div className="items-header px-4 pt-3">
             <h1 className='font-weight-light'>Items</h1>
-            <AddComment
+            <AddItem
                 value={props.value}
                 changeInput={props.changeInput}
                 saveItem={props.saveItem}
